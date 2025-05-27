c058c584858ced8e706e39082307d5bfb71582fb
B-Gayathrisingh <gayathrisingh810@gmail.com>
2025-05-27 11:30:31 +0530

    ok

--- register/src/main/java/com/register/register/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.register.register;
-
-public class Demo {
-
-    public static void main(String args[]){
-        System.out.println("Hello");
-    }
-
-}
--- register/src/main/java/com/register/register/repository/RegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.register.register.repository;
-
-import com.register.register.entity.Register;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterRepository extends JpaRepository<Register,Integer> {
-
-    Register findByemail(String email);
-}
